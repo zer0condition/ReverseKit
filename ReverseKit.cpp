60393937caddb3407b6ad51b8009376e00033131
ZeroCondition <ZeroCondition@DESKTOP-IFLP0N5>
2023-03-25 14:45:41 +0530

    Imports

--- ReverseKit.cpp
+++ ReverseKit.cpp
@@ -7,19 +7,58 @@
 
 unsigned char original_bytes[14];
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+void GetImportsFromIAT()
 {
+    auto hModule = GetModuleHandle(NULL);
+
+    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)hModule;
+    PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)((BYTE*)hModule + pDosHeader->e_lfanew);
+    PIMAGE_IMPORT_DESCRIPTOR  pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((BYTE*)hModule + pNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+
+    DWORD* pAddress;
+
+    while (pImportDesc->Name)
+    {
+        PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)((BYTE*)hModule + pImportDesc->FirstThunk);
+        PIMAGE_THUNK_DATA pThunkOrig = (PIMAGE_THUNK_DATA)((BYTE*)hModule + pImportDesc->OriginalFirstThunk);
+        while (pThunkOrig->u1.AddressOfData)
+        {
+            if (pThunkOrig->u1.Ordinal & IMAGE_ORDINAL_FLAG)
+            {
+                pAddress = (DWORD*)&pThunk->u1.Function;
+                printf("DLL: %s, Ordinal: %u, Address: %p\n", (char*)((BYTE*)hModule + pImportDesc->Name), IMAGE_ORDINAL(pThunkOrig->u1.Ordinal), (void*)*pAddress);
+            }
+            else
+            {
+                PIMAGE_IMPORT_BY_NAME pImportByName = (PIMAGE_IMPORT_BY_NAME)((BYTE*)hModule + pThunkOrig->u1.AddressOfData);
+                pAddress = (DWORD*)&pThunk->u1.Function;
+                printf("DLL: %s, Function: %s, Address: %p\n", (char*)((BYTE*)hModule + pImportDesc->Name), pImportByName->Name, (void*)*pAddress);
+            }
+            pThunk++;
+            pThunkOrig++;
+        }
+
+        pImportDesc++;
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
         SetConsoleTitleA("ReverseKit Attached");
 
+        // Imports info
+        GetImportsFromIAT();
+
+        // Place hooks
         oCreateProcessInternalW = (CreateProcessInternalW_t)GetProcAddress(GetModuleHandleA("kernelbase.dll"), "CreateProcessInternalW");
         ReverseHook::hook(oCreateProcessInternalW, hkCreateProcessInternalW, original_bytes);
+
         break;
 
     case DLL_PROCESS_DETACH:
-       ReverseHook::unhook(oCreateProcessInternalW, original_bytes);
+        ReverseHook::unhook(oCreateProcessInternalW, original_bytes);
         break;
     }
     return TRUE;
