057912aac68fb21fcc6c815512d10c7ecd1f04f5
ZeroCondition <88311365+zer0condition@users.noreply.github.com>
2023-03-25 14:13:44 +0530

    Update ReverseKit.cpp

--- ReverseKit.cpp
+++ ReverseKit.cpp
@@ -19,7 +19,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
         break;
 
     case DLL_PROCESS_DETACH:
-       ReverseHook::unhook(oCreateProcessInternalW, original_bytes);
+        ReverseHook::unhook(oCreateProcessInternalW, original_bytes);
         break;
     }
     return TRUE;
