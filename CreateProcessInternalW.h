c2d9f2e7d7270e19441b752628a22ec528792ffd
ZeroCondition <ZeroCondition@DESKTOP-IFLP0N5>
2023-03-25 21:07:17 +0530

    revamp

--- CreateProcessInternalW.h
+++ CreateProcessInternalW.h
@@ -30,18 +30,7 @@ BOOL WINAPI hkCreateProcessInternalW(
     LPPROCESS_INFORMATION lpProcessInformation,
     PHANDLE hNewToken)
 {
-
-    char message[256];
-    sprintf(message, "Block Commands Intercepted: %ls?", lpCommandLine);
-
-    int result = MessageBoxA(NULL, message, "[ReverseKit] CreateProcessInternalW", MB_YESNO);
-
-    if (result == IDYES) {
-        printf("[ReverseKit] Blocked Commands: %ls\n", lpCommandLine);
-        return TRUE;
-    }
-
-    printf("[ReverseKit] Accepted Commands: %ls\n", lpCommandLine);
+    printf("[ReverseKit] Commands Intercepted: %ls\n", lpCommandLine);
 
     return oCreateProcessInternalW(hUserToken, lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation, hNewToken);
 }
